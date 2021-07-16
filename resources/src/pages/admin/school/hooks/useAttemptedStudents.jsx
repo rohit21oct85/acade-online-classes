c570fe229e7d251dd7322ace8399ec53793e21ec
Rohit Singh <rohit21oct85@gmail.com>
2021-07-16 13:57:52 +0530

    mock test report add empid, rollno

--- resources/src/pages/admin/school/hooks/useAttemptedStudents.jsx
+++ resources/src/pages/admin/school/hooks/useAttemptedStudents.jsx
@@ -1,4 +1,4 @@
-import {useContext}  from 'react'
+import {useContext, useState}  from 'react'
 import {useQuery} from 'react-query';
 import axios from 'axios';
 import {AuthContext} from '../../../../context/AuthContext.jsx';
@@ -8,6 +8,7 @@ import { useParams } from 'react-router-dom';
 export default function useAttemptedStudents() {
     const {state } = useContext(AuthContext);
     const params = useParams();
+    const [intervalMs, setIntervalMs] = useState(1000)
     return useQuery(`attempted_studetns-${params?.test_id}`, async () => {
         if(state.access_token && params?.test_id && params?.test_type){
             const result = await axios.get(`${API_URL}v1/test-report/view-student/${params?.test_id}/${params?.school_id}/${params?.class_id}/${params?.test_type}`,{
@@ -15,6 +16,8 @@ export default function useAttemptedStudents() {
                     'Content-Type': 'Application/json',
                     'Authorization':'Bearer '+state.access_token
                 }
+            },{
+                refetchInterval: intervalMs,
             });
             return result.data.data; 
         }
